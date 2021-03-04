c7c80963324d0721491e5e496012d1ed893cf4df
Luis Luna <luiquicash@gmail.com>
2021-03-04 11:59:01 -0400

    .

--- Capa de Presentacion/frmListadoVentas.Designer.cs
+++ Capa de Presentacion/frmListadoVentas.Designer.cs
@@ -752,7 +752,6 @@
             this.button3.Text = "Eliminar";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button3.UseVisualStyleBackColor = false;
-            this.button3.Visible = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // frmListadoVentas
