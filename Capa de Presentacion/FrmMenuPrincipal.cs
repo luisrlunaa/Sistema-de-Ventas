7ea2a842baba64d017111e2f07bdd7a03da137f5
Luis Luna <luiquicash@gmail.com>
2021-03-04 08:55:10 -0400

    entrada a cuadre de caja

--- Capa de Presentacion/FrmMenuPrincipal.cs
+++ Capa de Presentacion/FrmMenuPrincipal.cs
@@ -267,7 +267,7 @@ namespace Capa_de_Presentacion
 
 		private void pictureBox1_Click(object sender, EventArgs e)
 		{
-			if (Program.LoginStatus !="" || Program.LoginStatus != null)
+			if (Program.LoginStatus !="" && Program.LoginStatus != null)
 			{
 				Program.LoginStatus = "";
 				FrmLogin login = new FrmLogin();
