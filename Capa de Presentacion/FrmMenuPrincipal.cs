b881c89594a3a6e4a100329b7ea9a26f31765ad5
Luis Luna <luiquicash@gmail.com>
2021-04-28 10:10:32 -0400

    resolviendo problemas de abrir ventanas

--- Capa de Presentacion/FrmMenuPrincipal.cs
+++ Capa de Presentacion/FrmMenuPrincipal.cs
@@ -243,6 +243,8 @@ namespace Capa_de_Presentacion
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            Program.abiertosecundario = false;
+            Program.abierto = false;
             if (Program.LoginStatus != "" && Program.LoginStatus != null)
             {
                 Program.LoginStatus = "";
@@ -266,14 +268,13 @@ namespace Capa_de_Presentacion
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (Program.abierto == false)
-            {
+
                 frmTurno Tu = new frmTurno();
                 Tu.lblLogo.Text = lblLogo.Text;
                 Tu.textBox2.Text = Program.turno + "";
-                Program.abierto = true;
-                Tu.Show();
-            }
+            Program.abiertosecundario = false;
+            Program.abierto = false;
+            Tu.Show();
         }
         private void button2_Click(object sender, EventArgs e)
         {
--- Capa de Presentacion/frmListadoVentas.cs
+++ Capa de Presentacion/frmListadoVentas.cs
@@ -451,6 +451,7 @@ namespace Capa_de_Presentacion
         private void dataGridView1_DoubleClick(object sender, EventArgs e)
         {
             Program.abiertosecundario = false;
+            Program.abierto = false;
             seleccion_data();
             FrmRegistroVentas V = new FrmRegistroVentas();
             V.btnSalir.Visible = false;
