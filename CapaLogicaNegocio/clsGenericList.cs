63419cb42da0394d3736669078f103174f0531c7
Luiqui Cash <luisrlunaa@outlook.com>
2022-03-19 09:22:16 -0400

    TempData

--- CapaLogicaNegocio/clsGenericList.cs
+++ CapaLogicaNegocio/clsGenericList.cs
@@ -95,4 +95,9 @@ namespace CapaLogicaNegocio
         }
         #endregion
     }
+    public class TempData
+    {
+        public static List<Venta> tempSalesData { get; set; }
+        public static DateTime DateIn { get; set; }
+    }
 }
