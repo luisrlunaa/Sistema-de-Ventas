721a38d38a2a0722c168cbb06a5bbffad6d7f6da
Luiqui Cash <luisrlunaa@outlook.com>
2022-03-18 22:03:53 -0400

    Cambiando nombre a proyecto

--- Capa de Presentacion/Properties/AssemblyInfo.cs
+++ Capa de Presentacion/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // La información general sobre un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos atributos para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Ferreteria Almonte")]
+[assembly: AssemblyTitle("Colchoneria y Distribuidora Rodriguez")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LunasSystem")]
