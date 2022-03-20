34f30e474f93c89424b85b2afb561fe487155ddd
Luiqui Cash <luisrlunaa@outlook.com>
2022-03-20 16:17:17 -0400

    realizando venta de una cotizacion

--- Capa de Presentacion/FrmRegistroVentas.cs
+++ Capa de Presentacion/FrmRegistroVentas.cs
@@ -1041,6 +1041,7 @@ namespace Capa_de_Presentacion
                     frmlistadodecotizacion F = new frmlistadodecotizacion();
                     Program.abiertosecundarias = false;
                     Program.abierto = false;
+                    button3.Visible = true;
                     F.Show();
                 }
             }
