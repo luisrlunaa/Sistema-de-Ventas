4fb50a5d894e96f9c331a42c2f72e6fdbd1b8823
luiquicash <47165717+luiquicash@users.noreply.github.com>
2022-04-27 22:46:49 -0400

    .....

--- Capa de Presentacion/CrearTiket.cs
+++ Capa de Presentacion/CrearTiket.cs
@@ -191,7 +191,7 @@ namespace Capa_de_Presentacion
         public void EncabezadoVenta()
         {
             //Escribimos los espacios para mostrar el articulo. En total tienen que ser 40 caracteres
-            linea.AppendLine("ARTICULOS             |CANTxPRECIO |SUBT |ITBIS");
+            linea.AppendLine("ARTICULOS            |CANTxPRECIO |SUBT |ITBIS");
         }
 
         //Metodo para agregar los totales d ela venta
