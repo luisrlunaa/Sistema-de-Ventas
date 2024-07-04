3ad142f3b4a83f4856b0ce84bc8564fea73ebf59
Luis Ricardo Luna <47165717+luisrlunaa@users.noreply.github.com>
2024-07-03 20:19:11 -0400

    TiendaCelular

--- Capa de Presentacion/FrmMenuPrincipal.cs
+++ Capa de Presentacion/FrmMenuPrincipal.cs
@@ -1,11 +1,9 @@
 ﻿using CapaEnlaceDatos;
 using CapaLogicaNegocio;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
