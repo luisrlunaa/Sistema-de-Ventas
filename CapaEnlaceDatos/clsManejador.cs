8a6d165135c5ca4f62a39bda1c777148e7c8a4f2
Luis Ricardo Luna <47165717+luisrlunaa@users.noreply.github.com>
2024-07-24 21:28:54 -0400

    conection from file

--- CapaEnlaceDatos/clsManejador.cs
+++ CapaEnlaceDatos/clsManejador.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace CapaEnlaceDatos
 {
     public class clsManejador
     {
-        public SqlConnection conexion = new SqlConnection("Data Source=.;Initial Catalog=SalesSystem;Integrated Security=True");
+        public SqlConnection conexion = new SqlConnection(LeerConexionDesdeArchivo());
+
         public void Conectar()
         {
             if (conexion.State == ConnectionState.Closed)
@@ -77,6 +79,29 @@ namespace CapaEnlaceDatos
                 throw ex;
             }
             Desconectar();
+        }
+
+        public static string LeerConexionDesdeArchivo()
+        {
+            string filePath = @"C:\conexion.txt";
+            var conn = string.Empty;
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    conn = File.ReadAllText(filePath);
+                }
+                else
+                {
+                    Console.WriteLine("El archivo de conexión no se encontró.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al leer el archivo de conexión: " + ex.Message);
+            }
+
+            return conn;
         }
     }
 }
