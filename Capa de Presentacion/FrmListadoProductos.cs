d06cfe621859aebd8aa3b6be32878b35da739a46
Luis Ricardo Luna <47165717+luisrlunaa@users.noreply.github.com>
2024-12-13 08:49:13 -0400

    use pdf helpers

--- Capa de Presentacion/FrmListadoProductos.cs
+++ Capa de Presentacion/FrmListadoProductos.cs
@@ -481,40 +481,7 @@ namespace Capa_de_Presentacion
 
         public void GenerarDocumento(Document document)
         {
-            int i, j;
-            PdfPTable datatable = new PdfPTable(dataGridView1.ColumnCount);
-            float[] headerwidths = GetTamañoColumnas(dataGridView1);
-            datatable.SetWidths(headerwidths);
-            datatable.WidthPercentage = 100;
-            datatable.DefaultCell.BorderWidth = 1;
-            datatable.DefaultCell.HorizontalAlignment = Element.ALIGN_LEFT;
-            for (i = 0; i < dataGridView1.ColumnCount; i++)
-            {
-                datatable.AddCell(new Phrase(dataGridView1.Columns[i].HeaderText, FontFactory.GetFont("ARIAL", 7, iTextSharp.text.Font.BOLD)));
-            }
-            datatable.HeaderRows = 1;
-            datatable.DefaultCell.BorderWidth = 1;
-            for (i = 0; i < dataGridView1.Rows.Count; i++)
-            {
-                for (j = 0; j < dataGridView1.Columns.Count; j++)
-                {
-                    if (dataGridView1[j, i].Value != null)
-                    {
-                        datatable.AddCell(new Phrase(dataGridView1[j, i].Value.ToString(), FontFactory.GetFont("ARIAL", 6, iTextSharp.text.Font.NORMAL)));//En esta parte, se esta agregando un renglon por cada registro en el datagrid
-                    }
-                }
-                datatable.CompleteRow();
-            }
-            document.Add(datatable);
-        }
-        public float[] GetTamañoColumnas(DataGridView dg)
-        {
-            float[] values = new float[dg.ColumnCount];
-            for (int i = 0; i < dg.ColumnCount; i++)
-            {
-                values[i] = (float)dg.Columns[i].Width;
-            }
-            return values;
+            Pdf.GenerarDocumento(document, dataGridView1);
         }
 
         private void btnimpimir_Click(object sender, EventArgs e)
